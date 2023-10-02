8f3224d625e5e4943220ba3b6b6b618da6fb6f63
Hong, SeokHyeon <tyuiop929@gmail.com>
2023-10-03 00:59:45 +0900

    feta:Add

---
+++ week03/dbendpagination/src/main/java/com/codingbottle/dbendpagination/domain/studentinlecture/StudentInLectureRepository.java
@@ -0,0 +1,6 @@
+package com.codingbottle.dbendpagination.domain.studentinlecture;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentInLectureRepository extends JpaRepository<StudentInLecture, Long> {
+}
