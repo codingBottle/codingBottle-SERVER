767c77c9dc4a25078f597e652973cac77a2c25eb
Cho, Yonghyun <chooh1010@gamil.com>
2023-10-06 21:07:31 +0900

    3주차 과제

--- week03/yonghyun/dbendpagination/src/main/java/com/codingbottle/dbendpagination/domain/student/Student.java
+++ week03/yonghyun/dbendpagination/src/main/java/com/codingbottle/dbendpagination/domain/student/Student.java
@@ -1,11 +1,13 @@
 package com.codingbottle.dbendpagination.domain.student;
 
+import com.codingbottle.dbendpagination.domain.studentinlecture.Penalty;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,9 +20,17 @@ public class Student {
     @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private Integer totalPenalty;
+
     @Builder
     private Student(String name) {
         this.name = name;
+        totalPenalty = 0;
+    }
+
+    public void updateTotalPenalty(Integer penalty){
+        totalPenalty += penalty;
     }
 
 
